ae29b810a496f7fbb4cf3065c61923699e722975
titosco <titobiloluwatella@gmail.com>
2024-07-31 06:18:34 +0100

    adding script

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 ### New version of TECH-BUUDY
+
+### create routes directories
+
+in it create an api directories
+create users.js- handles registering users,add users
+auth.js-gets json webtoken for auth
+profiles.js- handles creating profiles, adding them, updating them, deleting them
+posts.js- creates forms areas where we can like posts, comment on them
+
+### create models
+
+1st User model
+include name username password data
+type: string, date
+required: true
+unique: true
+
+### create user modle
+
+see if user exist
+grab user gravater
+return jsonwebntoken usign bcrypt
+
+### bcrypt
+
+create a salt
+
+### create middleware
+
+create auth.js that takes in req, res,next
+
+### login user
+
+login user and use bycrypt to compare password inputed and the actual password stored
+
+### Create profile model
+
+### AI integration, Promt engineering, AI assimulation
---
+++ routes/api/profile.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const {check, validationResult} = require('express-validator');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route   GET api/profile/me
+// @des     Get current users profile
+// @access  private
+
+router.get('/me', auth, async(req, res) => {
+    // get profile of private user not all user
+    try{
+        const profile = await Profile.findOne({ user: req.user.id}).populate('user', ['name', 'avater']);
+
+        if(!profile){
+            return res.status(400).json({msg : 'there is no profile for this user'});
+        }
+
+         res.json(profile)
+    } catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   POST api/profile
+// @des    Create or update user profile
+// @access  private
+router.post('/', [auth, [
+    check('status', 'Status is required').not().isEmpty(),
+    check('skills', 'Skills is required').not().isEmpty()
+]], async(req, res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array()});
+    }
+
+    const{
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githubusername,
+        skills,
+        youtube,
+        facebook,
+        twitter,
+        instagram,
+        linkedin
+    } = req.body
+
+    // Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if(company) profileFields.company = company;
+    if(website) profileFields.website = website;
+    if(location) profileFields.location = location;
+    if(bio) profileFields.bio = bio;
+    if(status) profileFields.status = status;
+    if(githubusername) profileFields.githubusername = githubusername;
+    if(skills){
+        profileFields.skills = skills.split(',').map(skill => skill.trim());
+    }
+
+    // build social objsct
+    profileFields.social = {}
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+
+    console.log(profileFields.skills);
+
+    
+    try {
+        let profile = await Profile.findOne({ user: req.user.id});
+
+        if(profile){
+            // update
+            profile = await Profile.findOneAndUpdate(
+                { user: req.user.id},
+                { $set: profileFields},
+                {new: true}
+            );
+            return res.json(profile);    
+        };
+        // create
+        profile = new Profile(profileFields);
+
+        await profile.save();
+        res.json(profile)
+        
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error');
+    }
+})
+
+module.exports = router;
